9d3549c92e19c5db7ea01eda6c2427e5b534c7c1
kuroye <erhanaket77@gmail.com>
2023-09-30 21:54:34 +0600

    changes on models

--- backend/apps/order/models.py
+++ backend/apps/order/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+from user.models import User
+from product.models import Product
 
 # Create your models here.
 
 class Order(models.Model):
 
-    # 过会写
-    # payment_status = models.BooleanField(default=False)
-    # order = models.ForeignKey()
-    # seller = 
-    # buyer = 
-    pass
+    text = models.TextField(null=True)
+    payment_status = models.BooleanField(default=False)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    buyer = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.product) + str(self.buyer)
--- backend/apps/product/models.py
+++ backend/apps/product/models.py
@@ -7,6 +7,8 @@ class Product(models.Model):
     title = models.CharField(max_length=200, null=False, default='default product')
     price = models.FloatField(default=0, null=False)
 
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.title
 
