26620c7985b727f93ba577b152de391ac1c06072
kuroye <erhanaket77@gmail.com>
2023-09-30 12:54:02 +0600

    Comment model

--- backend/apps/product/models.py
+++ backend/apps/product/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from user.models import User 
 # Create your models here.
 
 class Product(models.Model):
@@ -9,3 +9,13 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+
+    text = models.TextField(null=False)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return str(self.product)+' '+ str(self.user)
