6094ece0eac318bc48fd7b433afda9c88c1d9cf3
kuroye <erhanaket77@gmail.com>
2023-09-30 12:22:33 +0600

    product model

--- backend/apps/product/models.py
+++ backend/apps/product/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    
+    title = models.CharField(max_length=200, null=False, default='default product')
+    price = models.FloatField(default=0, null=False)
+
+    def __str__(self):
+        return self.title
