a1500349f114e60bd37655c0103860a214af83bd
kuroye <erhanaket77@gmail.com>
2023-09-30 13:10:41 +0600

    Order model

--- backend/apps/order/models.py
+++ backend/apps/order/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Order(models.Model):
+
+    # 过会写
+    # payment_status = models.BooleanField(default=False)
+    # order = models.ForeignKey()
+    # seller = 
+    # buyer = 
+    pass
