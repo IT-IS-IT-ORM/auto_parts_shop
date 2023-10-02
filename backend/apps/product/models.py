67432c2d285e8a974c217d611f45e1089077251c
kuro_ye <erhanaket77@gmail.com>
2023-10-02 13:45:31 +0600

    数据库 pizdes

--- backend/apps/product/models.py
+++ backend/apps/product/models.py
@@ -2,36 +2,44 @@ from django.db import models
 from user.models import User 
 # Create your models here.
 
-class Product(models.Model):
-    
-    title = models.CharField(max_length=200, null=False, default='default product')
-    price = models.FloatField(default=0, null=False)
 
-    seller = models.ForeignKey(User, on_delete=models.CASCADE)
+class Category(models.Model):
+
+    title = models.CharField(max_length=200, null=False, default='default category')
+    parent = models.ForeignKey("self", on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.title
 
+class ProductGallery(models.Model):
+
+    # 图片field goes here，保存路径我不确定 先留着
+    pass
+
+
+class Product(models.Model):
+    
+    title = models.CharField(max_length=200, null=False, default='default product')
+    price = models.FloatField(default=0, null=False)
+    is_new = models.BooleanField(default=False)
+
+    # gallery = models.ManyToManyField(ProductGallery)
+
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+
+    applicable = models.ManyToManyField(Category, related_name='applicables', null=True)
+
+
+
+
 class Comment(models.Model):
 
     text = models.TextField(null=False)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
     def __str__(self):
         return str(self.product)+' '+ str(self.user)
-
-class Category(models.Model):
-
-    title = models.CharField(max_length=200, null=False, default='default category')
-    parent = models.ForeignKey("self", on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title
-
-
-# class CategoryChild(models.Model):
-
-#     title = models.CharField(max_length=200, null=False, default='default category')
-#     father = models.ForeignKey
