cbfd117156536656b8cbf751acc9ef5572fb8865
kuroye <erhanaket77@gmail.com>
2023-10-03 22:46:01 +0600

    Order API view and url

--- backend/apps/order/urls.py
+++ backend/apps/order/urls.py
@@ -1,20 +1,6 @@
 from django.urls import path
-from .views import OrderViewSet
+from .views import OrderAPIView
 
-from rest_framework import routers
-from django.conf import settings
-
-
-if settings.DEBUG:
-    router = routers.DefaultRouter()
-else:
-    router = routers.SimpleRouter()
-
-router.register(r'', OrderViewSet)
-
-urlpatterns = router.urls
-
-
-urlpatterns += [
-    # path('', ProductAPIView.as_view())
+urlpatterns = [
+    path('', OrderAPIView.as_view())
 ]
