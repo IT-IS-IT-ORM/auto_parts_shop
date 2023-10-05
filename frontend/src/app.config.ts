4ac639ca300e4ad2df18186ebb02dca34ba7fc78
YernarT <toktaryernar@gmail.com>
2023-10-05 12:33:52 +0600

    update: :hammer: 修改主题色

--- frontend/src/app.config.ts
+++ frontend/src/app.config.ts
@@ -2,7 +2,7 @@ export default defineAppConfig({
   nuxtIcon: {
     size: "24px",
     class: "itisit-icon",
-    color: "#23e5db",
+    color: "#002F34",
     aliases: {
       nuxt: "logos:nuxt-icon",
     },
