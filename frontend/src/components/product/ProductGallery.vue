7f777644b8d5f4768482085fd0a4110f9d327118
YernarT <toktaryernar@gmail.com>
2023-10-13 23:34:32 +0600

    init: :rainbow: 商品相册

---
+++ frontend/src/components/product/ProductGallery.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="product-gallery">
+
+    </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+@import "~/assets/style/mixins.scss";
+
+.product-gallery {
+    padding: 24px;
+    border-radius: var(--border-radius);
+    background-color: #fff;
+    position: relative;
+}
+</style>
