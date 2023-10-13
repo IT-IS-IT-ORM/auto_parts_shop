ae257d85edd7c359b5d624c76a08f4b6b5de9f10
YernarT <toktaryernar@gmail.com>
2023-10-13 23:23:51 +0600

    refactor: :recycle: 模块重构

--- backend/apps/product/serializers.py
+++ backend/apps/product/serializers.py
@@ -1,59 +1,59 @@
 from rest_framework import serializers
 
-from .models import Product, Category, ProductGallery
-
+from product.models import Product, Category, ProductImage
 from user.models import User, Favorite
 from user.serializers import UserSerializer
+
 
 class CategorySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Category
-        fields = ['id', 'title']
+        fields = '__all__'
+
 
 class ProductSerializer(serializers.ModelSerializer):
+    seller = UserSerializer()
 
     class Meta:
         model = Product
         fields = '__all__'
 
     def to_representation(self, instance):
-
-
         product = super().to_representation(instance)
 
-        seller =  User.objects.get(id=product['seller'])
-        
         subCategory = Category.objects.get(id=product['category'])
         category = Category.objects.get(id=subCategory.parent.id)
         categories = []
         categories.append(category)
         categories.append(subCategory)
 
-
-        product['seller'] = UserSerializer(seller).data
-        #消除token
-        del product['seller']['token']
-
         # category自定义
         product['category'] = CategorySerializer(categories, many=True).data
-        
 
         # applicable里自定义 没有用到serializer 而是调用Model里的__str__函数
         for index, item in enumerate(product['applicable']):
             applicable = Category.objects.get(id=item)
-            product['applicable'][index] = {'id': applicable.id, 'title':str(applicable)}
+            product['applicable'][index] = {'id': applicable.id,
+                                            'title': str(applicable)}
 
-        for index, item in enumerate(product['gallery']):
-            image = ProductGallery.objects.get(id=item)
-            product['gallery'][index] = {'id': image.id, 'name': image.name, 'image': image.image.url}
+        # gallery自定义
+        gallery = ProductImage.objects.filter(product=product['id'])
+        product['gallery'] = list(map(lambda image: ProductImageSerializer(instance=image).data, gallery))
 
         return product
-    
+
+
+
+class ProductImageSerializer(serializers.ModelSerializer):
+    product = serializers.PrimaryKeyRelatedField(queryset=ProductImage.objects.all(), write_only=True)
+    class Meta:
+        model = ProductImage
+        fields = '__all__'
 
 class FavoriteSerializer(serializers.ModelSerializer):
-
     product = ProductSerializer()
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), write_only=True)
 
     class Meta:
         model = Favorite
