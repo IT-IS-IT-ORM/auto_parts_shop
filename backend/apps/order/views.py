43e36e2d020d215e339dbc38b137d06225b2c17a
YernarT <toktaryernar@gmail.com>
2023-10-13 22:54:38 +0600

    update: :hammer: 简化代码

--- backend/apps/order/views.py
+++ backend/apps/order/views.py
@@ -1,13 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.response import Response
 
-from .models import Order
-from .serializers import OrderSerializer
-
+from order.models import Order
+from order.serializers import OrderSerializer
 
 from utils.authentication import LoginRequiredAuthentication
-
-# Create your views here.
 
 
 class OrderAPIView(ModelViewSet):
@@ -17,16 +13,9 @@ class OrderAPIView(ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
 
-
     def get_queryset(self):
-       #用户是个球
-       if int(self.request.user.role) == 1:
-        return Order.objects.filter(buyer=self.request.user)
-       else:
-        return Order.objects.filter(product__seller=self.request.user)
-
-    def get(self, request):
-        order = Order.objects.all()
-        serializer = OrderSerializer(order, many=True, context={
-                                     'user': self.request.user})
-        return Response(serializer.data)
+        user = self.request.user
+        if user.role == '1':
+            return Order.objects.filter(buyer=user)
+        else:
+            return Order.objects.filter(product__seller=user)
