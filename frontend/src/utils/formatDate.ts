61a83babcd174bd05caaf8c94a334d1ae36591f5
YernarT <toktaryernar@gmail.com>
2023-10-17 01:13:05 +0600

    update: :hammer: 强化 Sunkar 的时间格式化函数

--- frontend/src/utils/formatDate.ts
+++ frontend/src/utils/formatDate.ts
@@ -1,57 +1,33 @@
+function getCurrentMonth(date: Date, fullversion: boolean) {
+  const months = [
+    ["қаң", "қаңтар"],
+    ["ақп", "ақпан"],
+    ["нау", "науырыз"],
+    ["сәу", "сәуір"],
+    ["мам", "мамыр"],
+    ["мау", "маусым"],
+    ["шіл", "шілде"],
+    ["там", "тамыз"],
+    ["қыр", "қыркүйек"],
+    ["қаз", "қазан"],
+    ["қар", "қараша"],
+    ["жел", "желтоқсан"],
+  ];
 
+  const currentMonth = date.getMonth();
+  return months[currentMonth][Number(fullversion)];
+}
 
-const date = "2023-12-13T20:57:52.504106+06:00";
-
-function dateFormatter(dateString) {
+export default function dateFormatter(
+  dateString: string,
+  fullversion: boolean
+) {
   const date = new Date(dateString);
-  const month = getCurrentMonth(date);
+  const monthStr = getCurrentMonth(date, fullversion);
 
-  return `${date.getDate()} ${month}.`;
+  if (fullversion) {
+    return `${date.getDate()} ${monthStr} ${date.getFullYear}`;
+  }
+
+  return `${date.getDate()} ${monthStr}.`;
 }
-
-function getCurrentMonth(date) {
-  const reflect = {
-    1() {
-      return "Қаң";
-    },
-    2() {
-      return "Ақп";
-    },
-    3() {
-      return "Нау";
-    },
-    4() {
-      return "Сәу";
-    },
-    5() {
-      return "Мам";
-    },
-    6() {
-      return "Мау";
-    },
-    7() {
-      return "Шіл";
-    },
-    8() {
-      return "Там";
-    },
-    9() {
-      return "Қыр";
-    },
-    10() {
-      return "Қаз";
-    },
-    11() {
-      return "Қар";
-    },
-    12() {
-      return "Жел";
-    },
-  };
-
-  // 月份从0开始，所以要加1
-  const currentMonth = date.getMonth() + 1;
-  return reflect[currentMonth]();
-}
-
-console.log(dateFormatter(date));
