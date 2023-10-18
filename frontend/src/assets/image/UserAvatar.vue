099d80ecaf47388273d05339e35f5710a99dccbf
YernarT <toktaryernar@gmail.com>
2023-10-18 13:11:55 +0600

    feat: :sparkles: user avatar

---
+++ frontend/src/assets/image/UserAvatar.vue
@@ -0,0 +1,40 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="1em" height="1em"
+        viewBox="0 0 71 71" data-testid="user-photo-placeholder" alt="User avatar" class="css-11963t">
+        <defs>
+            <path id="avatar_svg__a"
+                d="M.029 34.9c0 19.272 15.623 34.896 34.896 34.896 19.273 0 34.896-15.624 34.896-34.897C69.821 15.627 54.198.003 34.925.003 15.652.003.03 15.627.03 34.9">
+            </path>
+            <path id="avatar_svg__c"
+                d="M0 34.99c0 19.272 15.624 34.896 34.896 34.896 19.273 0 34.896-15.624 34.896-34.896C69.792 15.717 54.17.094 34.896.094 15.624.094 0 15.717 0 34.99z">
+            </path>
+        </defs>
+        <g fill="none" fill-rule="evenodd">
+            <g transform="translate(.903 .903)">
+                <mask id="avatar_svg__b" fill="#fff">
+                    <use xlink:href="#avatar_svg__a"></use>
+                </mask>
+                <path fill="#3A77FF"
+                    d="M69.821 34.9c0 19.272-15.623 34.896-34.896 34.896C15.652 69.796.03 54.172.03 34.899.029 15.627 15.652.003 34.925.003c19.273 0 34.896 15.624 34.896 34.896"
+                    mask="url(#avatar_svg__b)"></path>
+            </g>
+            <g transform="translate(.932 .906)">
+                <mask id="avatar_svg__d" fill="#fff">
+                    <use xlink:href="#avatar_svg__c"></use>
+                </mask>
+                <use fill="#D8DFE0" xlink:href="#avatar_svg__c"></use>
+                <path fill="#406367" d="M10.788 98.23h48.216V50.013H10.788z" mask="url(#avatar_svg__d)"></path>
+                <path fill="#EBEEEF"
+                    d="M53.436 34.896c0 10.24-8.3 18.54-18.54 18.54-10.239 0-18.54-8.3-18.54-18.54 0-10.239 8.301-18.54 18.54-18.54 10.24 0 18.54 8.301 18.54 18.54"
+                    mask="url(#avatar_svg__d)"></path>
+                <path fill="#7F9799"
+                    d="M44.812 27.96c-3.975 5.115-11.344 6.04-16.46 2.065-5.115-3.975-6.04-11.344-2.065-16.46 3.975-5.115 11.344-6.04 16.46-2.065 5.116 3.975 6.04 11.344 2.065 16.46"
+                    mask="url(#avatar_svg__d)"></path>
+                <path fill="#7F9799"
+                    d="m44.547 28.301-9.604-7.462 7.463-9.604 9.603 7.462zm-30.922-1.955a7.617 7.617 0 1 1 15.115-1.897 7.617 7.617 0 0 1-15.115 1.897"
+                    mask="url(#avatar_svg__d)"></path>
+                <path fill="#7F9799" d="m13.59 26.067 7.836-.983.983 7.836-7.836.983z" mask="url(#avatar_svg__d)"></path>
+            </g>
+        </g>
+    </svg>
+</template>
