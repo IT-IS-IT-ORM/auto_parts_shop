2a95ac472111f54332f7ec0932b45b43eb8156b9
YernarT <toktaryernar@gmail.com>
2023-10-20 11:50:53 +0600

    refactor: :recycle: 重写收藏夹API

--- backend/apps/user/urls.py
+++ backend/apps/user/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from rest_framework import routers
 from django.conf import settings
 
-from user.views import UserViewSet, LoginAPIView, RegisterAPIView, ChangePasswordAPIView, FavoriteViewSet
+from user.views import UserViewSet, LoginAPIView, RegisterAPIView, ChangePasswordAPIView, FavoriteAPIView
 
 if settings.DEBUG:
     router = routers.DefaultRouter()
@@ -10,7 +10,6 @@ else:
     router = routers.SimpleRouter()
 
 router.register(r'user', UserViewSet)
-router.register(r'favorite', FavoriteViewSet)
 
 urlpatterns = router.urls
 
@@ -21,6 +20,8 @@ urlpatterns += [
     path('auth/register/', RegisterAPIView.as_view()),
     # Change password API
     path('auth/change-password/', ChangePasswordAPIView.as_view()),
+    # Favorite API
+    path('favorite/', FavoriteAPIView.as_view()),
 ]
 
 app_name = 'user'
