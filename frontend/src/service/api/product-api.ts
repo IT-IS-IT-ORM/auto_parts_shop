063944beebf9662d905e28385758ff27e2e8631c
YernarT <toktaryernar@gmail.com>
2023-10-20 11:52:29 +0600

    feat: :sparkles: favorite 模块

--- frontend/src/service/api/product-api.ts
+++ frontend/src/service/api/product-api.ts
@@ -8,12 +8,31 @@ import _fetch from "~/service/fetch";
 export const API_GetProductList = (): Promise<I_Response<I_Product[]>> =>
   _fetch.get("/product/");
 
+export const API_GetFavoriteProductList = (): Promise<
+  I_Response<I_Product[]>
+> => _fetch.get(`/favorite/`);
+
 export const API_GetProduct = (
   productId: number
 ): Promise<I_Response<I_Product>> => _fetch.get(`/product/${productId}/`);
 
-export const API_UpdateProduct = (): Promise<I_Response<I_Product>> =>
-  _fetch.patch("/product/");
+export const API_UpdateProduct = (
+  productId: number,
+  data: Partial<I_Product>
+): Promise<I_Response<I_Product>> =>
+  _fetch.patch(`/product/${productId}/`, data);
 
 export const API_AddProduct = (data: {}): Promise<I_Response<I_Product>> =>
   _fetch.post("/product/", data);
+
+export interface API_SetFavorite_Data {
+  productId: number;
+  isFavorite: boolean;
+}
+
+export const API_SetFavorite = (
+  data: API_SetFavorite_Data
+): Promise<I_Response<null>> =>
+  data.isFavorite
+    ? _fetch.post("/favorite/", { product: data.productId })
+    : _fetch.delete("/favorite/", { product: data.productId });
