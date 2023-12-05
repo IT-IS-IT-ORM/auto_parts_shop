55846d675f9a4a7014a394b9e5250738333faad2
YernarT <toktaryernar@gmail.com>
2023-12-05 18:44:14 +0600

    update: 解析数组

--- frontend/src/utils/objectToUrlParams.ts
+++ frontend/src/utils/objectToUrlParams.ts
@@ -1,10 +1,16 @@
 import _ from "lodash";
 
 export function objectToUrlParams(object: object) {
-  const params = _.map(
-    object,
-    (value, key) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`
-  );
+  const params = _.map(object, (value, key) => {
+    if (Array.isArray(value)) {
+      if ((value as []).filter((item) => item !== null).length === 0) {
+        return "";
+      }
+      return `${encodeURIComponent(key)}=${(value as []).join(",")}`;
+    }
+
+    return `${encodeURIComponent(key)}=${encodeURIComponent(value)}`;
+  });
 
   return params.join("&");
 }
