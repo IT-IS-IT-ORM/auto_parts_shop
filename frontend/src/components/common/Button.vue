e20f299dea1be0088fa58f02f29e4f112f500706
YernarT <toktaryernar@gmail.com>
2024-03-31 22:55:54 +0500

    update: remove PropType

--- frontend/src/components/common/Button.vue
+++ frontend/src/components/common/Button.vue
@@ -1,24 +1,24 @@
 <template>
-    <a-button class="custom-button" :class="[`custom-button--${variant}`, block && `custom-button--block`]">
-        <slot></slot>
-    </a-button>
+  <a-button
+    class="custom-button"
+    :class="[`custom-button--${variant}`, block && `custom-button--block`]"
+  >
+    <slot></slot>
+  </a-button>
 </template>
 
 <script setup lang="ts">
-// Vue
-import { PropType } from 'vue';
-
-export type allowedButtonVariants = 'primary' | 'secondary';
+export type allowedButtonVariants = "primary" | "secondary";
 
 defineProps({
-    block: {
-        type: Boolean,
-        default: false
-    },
-    variant: {
-        type: String as PropType<allowedButtonVariants>,
-        default: 'primary'
-    }
+  block: {
+    type: Boolean,
+    default: false,
+  },
+  variant: {
+    type: String,
+    default: "primary",
+  },
 });
 </script>
 
@@ -26,41 +26,41 @@ defineProps({
 @import "~/assets/style/mixins.scss";
 
 .custom-button {
-    height: auto;
-    padding: 4px 16px;
-    border: 4px solid transparent;
-    @include flexCenter;
+  height: auto;
+  padding: 4px 16px;
+  border: 4px solid transparent;
+  @include flexCenter;
 
-    :deep(span) {
-        font-weight: 500;
+  :deep(span) {
+    font-weight: 500;
+  }
+
+  &--block {
+    width: 100%;
+  }
+
+  &--primary {
+    color: #fff;
+    border-color: var(--c-primary);
+    background-color: var(--c-primary);
+
+    &:hover {
+      color: var(--c-primary);
+      border-color: var(--c-primary);
+      background-color: #fff;
     }
+  }
 
-    &--block {
-        width: 100%;
+  &--secondary {
+    color: var(--c-primary);
+    border-color: #fff;
+    background-color: #fff;
+
+    &:hover {
+      color: #fff;
+      border-color: #fff;
+      background-color: var(--c-primary);
     }
-
-    &--primary {
-        color: #fff;
-        border-color: var(--c-primary);
-        background-color: var(--c-primary);
-
-        &:hover {
-            color: var(--c-primary);
-            border-color: var(--c-primary);
-            background-color: #fff;
-        }
-    }
-
-    &--secondary {
-        color: var(--c-primary);
-        border-color: #fff;
-        background-color: #fff;
-
-        &:hover {
-            color: #fff;
-            border-color: #fff;
-            background-color: var(--c-primary);
-        }
-    }
+  }
 }
 </style>
