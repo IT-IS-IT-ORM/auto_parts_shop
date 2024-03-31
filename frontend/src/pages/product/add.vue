daa78729adbc3fc1beb477d842da9b188a985fe5
YernarT <toktaryernar@gmail.com>
2024-03-31 21:35:38 +0500

    init: :rainbow: add page

---
+++ frontend/src/pages/product/add.vue
@@ -0,0 +1,37 @@
+<template>
+    <main class="itisit-container product-add-page">
+        <AddProductForm />
+    </main>
+</template>
+
+<script setup lang="ts">
+// Vue
+import { defineComponent } from 'vue';
+// Components
+import AddProductForm from '~/components/product/AddProductForm.vue';
+
+defineComponent({ name: 'ProductAddPage' });
+</script>
+
+<style scoped lang="scss">
+@import "~/assets/style/mixins.scss";
+
+.product-add-page {
+    padding-top: 60px;
+    padding-bottom: 80px;
+    @include flex($direction: column, $gap: 40px);
+
+    @media screen and (max-width: 576px) {
+        padding-top: 40px;
+        padding-bottom: 60px;
+        gap: 24px;
+    }
+
+    @media screen and (max-width: 404px) {
+        padding-top: 24px;
+        padding-bottom: 40px;
+        gap: 20px;
+    }
+
+}
+</style>
