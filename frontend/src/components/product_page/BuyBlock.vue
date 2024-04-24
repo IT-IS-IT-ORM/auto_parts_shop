ee5999013932c839113725f8e096f6e7a3c42f9f
YernarT <toktaryernar@gmail.com>
2024-04-24 21:51:02 +0500

    style: :hammer: 格式化

--- frontend/src/components/product_page/BuyBlock.vue
+++ frontend/src/components/product_page/BuyBlock.vue
@@ -1,88 +1,97 @@
 <template>
-    <div class="buy-block">
-        <div class="row">
-            <p class="create-time">
-                Жарияланды {{ dateFormatter(product.createTime, true) }}
-            </p>
-            <Icon name="material-symbols:favorite-outline-rounded" role="button" />
-        </div>
-
-        <div class="name">
-            <div class="is-new">
-                {{ product.isNew ? 'Жаңа' : 'Ескі' }}
-            </div>
-            <h1>
-                {{ product.title }}
-            </h1>
-        </div>
-        <span class="price">{{ product.price }} тг</span>
-        <Button block>Заказ жасау</Button>
+  <div class="buy-block">
+    <div class="row">
+      <p class="create-time">
+        Жарияланды {{ dateFormatter(product.createTime, true) }}
+      </p>
+      <Icon name="material-symbols:favorite-outline-rounded" role="button" />
     </div>
+
+    <div class="name">
+      <div class="is-new">
+        {{ product.isNew ? "Жаңа" : "Ескі" }}
+      </div>
+      <h1>
+        {{ product.title }}
+      </h1>
+    </div>
+    <span class="price">{{ product.price }} тг</span>
+    <Button v-if="userStore.role === role.consumer" block>Заказ жасау</Button>
+  </div>
 </template>
 
 <script setup lang="ts">
 // Types
-import type { I_Product } from '~/types/product';
+import type { I_Product } from "~/types/product";
 
+// Store
+import { useUserStore } from "~/stores/user";
 // Utils
-import dateFormatter from '~/utils/formatDate';
+import { role } from "~/utils/role";
+import dateFormatter from "~/utils/formatDate";
 // Components
-import Button from '~/components/common/Button.vue';
+import Button from "~/components/common/Button.vue";
 
 defineProps<{
-    product: I_Product
+  product: I_Product;
 }>();
+
+const userStore = useUserStore();
 </script>
 
 <style scoped lang="scss">
 @import "~/assets/style/mixins.scss";
 
 .buy-block {
-    padding: 24px;
-    border-radius: var(--border-radius);
-    background-color: #fff;
-    @include flex($direction: column);
+  padding: 24px;
+  border-radius: var(--border-radius);
+  background-color: #fff;
+  @include flex($direction: column);
 
-    @media screen and (max-width: 576px) {
-        padding: 16px;
+  @media screen and (max-width: 576px) {
+    padding: 16px;
+  }
+
+  .row {
+    width: 100%;
+    @include flex(
+      $justifyContent: space-between,
+      $alignItems: center,
+      $gap: 8px
+    );
+
+    .create-time {
+      font-weight: 300;
+      font-size: 14px;
+      color: var(--c-text-secondary);
+    }
+  }
+
+  .name {
+    margin: 8px 0 16px;
+    @include flex($alignItems: center, $gap: 8px);
+
+    .is-new {
+      font-size: 16px;
+      padding: 6px 10px;
+      border-radius: var(--border-radius);
+      border: 1px solid var(--c-border);
     }
 
-    .row {
-        width: 100%;
-        @include flex($justifyContent: space-between, $alignItems: center, $gap: 8px);
-
-        .create-time {
-            font-weight: 300;
-            font-size: 14px;
-            color: var(--c-text-secondary);
-        }
+    h1 {
+      font-size: 18px;
+      @include maxRow($rowCount: 4, $rowHeight: 24px);
     }
+  }
 
-    .name {
-        margin: 8px 0 16px;
-        @include flex($alignItems: center, $gap: 8px);
+  .price {
+    font-weight: 500;
+    font-size: 22px;
+    margin-bottom: 16px;
+  }
 
-        .is-new {
-            font-size: 16px;
-            padding: 6px 10px;
-            border-radius: var(--border-radius);
-            border: 1px solid var(--c-border);
-        }
-
-        h1 {
-            font-size: 18px;
-            @include maxRow($rowCount: 4, $rowHeight: 24px);
-        }
-    }
-
-    .price {
-        font-weight: 500;
-        font-size: 22px;
-        margin-bottom: 16px;
-    }
-
-    .custom-button {
-        margin-top: auto;
-    }
+  .custom-button {
+    margin-top: auto;
+  }
 }
 </style>
