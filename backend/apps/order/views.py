2566d40c30e7c55056a2fbc3bf9bf129e569d4ce
YernarT <toktaryernar@gmail.com>
2024-04-24 22:15:51 +0500

    update: :hammer: create order logic

--- backend/apps/order/views.py
+++ backend/apps/order/views.py
@@ -1,7 +1,9 @@
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from order.models import Order
 from order.serializers import OrderSerializer
+from product.models import Product
 
 from utils.authentication import LoginRequiredAuthentication
 
@@ -19,3 +21,10 @@ class OrderAPIView(ModelViewSet):
             return Order.objects.filter(buyer=user)
         else:
             return Order.objects.filter(product__seller=user)
+
+    def create(self, request, *args, **kwargs):
+        product = Product.objects.get(id=request.data.get('product_id'))
+        order = Order.objects.create(price=product.price, product=product, buyer=request.user)
+        serializer = OrderSerializer(instance=order, context={'request': request})
+
+        return Response(data=serializer.data)
